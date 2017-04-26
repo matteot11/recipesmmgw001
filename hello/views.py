d3a278e919264b247c7cfac16621f988100b7b5f
matteot11 <matteo.tomei94@gmail.com>
2017-04-26 16:26:01 +0200

    ciao

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from models import Nationalities
 
 # Create your views here.
 def index(request):
     #return HttpResponse('Hello World!')
-    return render(request, 'index.html')
+    #return render(request, 'index.html')
+
+    nationalities = Nationalities.objects.all()
+    return render(request, 'index.html', {'nationalities': nationalities})
 
 #def index(request):
 #    r = requests.get('http://httpbin.org/status/418')
